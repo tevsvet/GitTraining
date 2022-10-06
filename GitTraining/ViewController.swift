1da1a77eff93bd506b597ed2cd21f1952519db8f
tevsvet <naumkina.alina601@gmail.com>
2022-10-06 22:14:53 +0300

    Initial Commit

---
+++ GitTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTraining
+//
+//  Created by Al Naumkina on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
